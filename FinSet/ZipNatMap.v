b39e0df9bc5f59ffd1c871735396f527e9802b91
Rupert Swarbrick <rswarbrick@gmail.com>
2020-08-29 15:58:05 +0100

    Prove that zip_nat_map has the right values

--- FinSet/ZipNatMap.v
+++ FinSet/ZipNatMap.v
@@ -22,6 +22,7 @@
 Require Import Lists.List.
 
 Require Import Top.FinSet.NatMap.
+Require Import Top.FinSet.Distinct.
 Require Import Top.FinSet.ProjSet.
 
 Set Implicit Arguments.
@@ -123,4 +124,65 @@ Section znm.
     : nat_map p1 p2 :=
     exist _ (top_map l fullH, bot_map fb l) (is_nat fb l fullH).
 
+  Definition pr_proj : A1 * A2 -> B1 := fun pr => p1 (fst pr).
+
+  (** How can we tell that we defined the "right" map? One obvious
+      thing to expect is that if the list contains a pair [(a1, a2)]
+      then [a1] is mapped to [a2]. Of course, that is only true if
+      there isn't some other pair that comes before and also maps to
+      [p1 a1]. This is guaranteed, however, if the first elements of
+      the pairs map to distinct elements. In our use case for
+      constructing maps between sets based on their cardinalities,
+      this will definitely hold.
+
+      Before proving the main result, we prove a simple lemma that
+      unpacks distinctness to show that what looks like a match
+      (because it's got the right projection to [B1]) really is a
+      match. *)
+
+  Lemma distinct_match_at a1 a2 a1' a2' l
+    : p1 a1' = p1 a1 ->
+      distinct (map pr_proj ((a1', a2') :: l)) ->
+      In (a1, a2) ((a1', a2') :: l) ->
+      (a1', a2') = (a1, a2).
+  Proof.
+    intros eq1H distinctH inH.
+    destruct distinctH as [ notinH _ ]; revert notinH.
+    unfold pr_proj at 1; simpl; rewrite eq1H; intro notinH.
+    destruct inH as [ | inH ]; auto.
+    contradiction notinH; exact (in_map pr_proj l (a1, a2) inH).
+  Qed.
+
+  Lemma distinct_zip_nat_map_at fb l fullH a1 a2
+    : distinct (map pr_proj l) ->
+      In (a1, a2) l ->
+      nm_top (zip_nat_map fb l fullH) a1 = a2.
+  Proof.
+    intros distinctH prInH.
+    unfold zip_nat_map; simpl.
+    unfold top_map, lift_pr_proj'.
+    generalize (fullH a1); intro inH1; clear fullH.
+    induction l as [ | pr l IH ]; [ contradiction inH1 | ].
+    destruct pr as [ a1' a2' ].
+    unfold InProj in inH1; simpl in inH1;
+      fold (InProj p1 (p1 a1) (map fst l)) in inH1.
+    unfold lift_staged_proj'; fold (lift_staged_proj' fst p1 decB1 (p1 a1) l).
+    unfold dec_in_inv; fold (map fst l); fold (map p1 (map fst l)).
+
+    destruct (decB1 (p1 a1') (p1 a1)) as [ eqpH | nepH ].
+    - assert ((a1', a2') = (a1, a2)) as eqH; eauto using distinct_match_at.
+      assert (a1' = a1) as eq1H; [ exact (f_equal fst eqH) | ].
+      assert (a2' = a2) as eq2H; [ exact (f_equal snd eqH) | ]; clear eqH.
+      destruct (decB1 (p1 (fst (a1', a2'))) (p1 a1)) as [ | neH ]; auto.
+      contradiction neH; simpl; apply f_equal; auto.
+    - destruct (decB1 (p1 (fst (a1', a2'))) (p1 a1)) as [ eq1H | neq1H ];
+        [ contradiction eq1H | ].
+      destruct (in_dec decB1 (p1 a1) (map p1 (map fst l))) as [ inH | notinH ].
+      * apply (IH (distinct_uncons distinctH)).
+        destruct prInH as [ eqH | ]; auto.
+        contradiction nepH; apply (f_equal p1); exact (f_equal fst eqH).
+      * contradiction notinH; destruct inH1; tauto.
+  Qed.
+
 End znm.
+
